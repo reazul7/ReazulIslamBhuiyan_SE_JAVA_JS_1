67ed3fb563451cd6632ffcb7c902ad3146ca4cfe
reazul7 <islamreazul7777@gmail.com>
2021-09-18 23:56:49 +0600

    all products with delete btn

--- src/components/Dashboard/DashboardContainer/AllProducts.js
+++ src/components/Dashboard/DashboardContainer/AllProducts.js
@@ -1,9 +1,105 @@
 import React from 'react';
 
+
 const AllProducts = () => {
     return (
-        <div>
-            <h1>All Products</h1>
+        <div class="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+                <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
+                <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
+                <div class="desc p-4 text-gray-800">
+                    <a href="#" target="_new" class="title font-bold block cursor-pointer">Pubg Mobile</a>
+                    <span class="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
+                    <div className="text-center">
+                        <button className="bg-blue-400 px-4 py-1 rounded-md shadow-md ">Delete</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
